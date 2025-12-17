ac0bf2b1b65fabbaec636f161bb060815ecc6aad
esratht <esratahtali@gmail.com>
2025-12-17 22:20:45 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  // output: 'export', // BU SATIR VARSA KESİNLİKLE SİL VEYA YORUMA AL
+  // images: { unoptimized: true }, // EĞER export KULLANMIYORSAN BUNA GEREK YOK
 }
 
 module.exports = nextConfig
