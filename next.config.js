bc89566edf75d1ed907907daad611c192c330dd9
esratht <esratahtali@gmail.com>
2025-12-20 15:28:35 +0300

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-  // output: 'export', // BU SATIR VARSA KESİNLİKLE SİL VEYA YORUMA AL
-  // images: { unoptimized: true }, // EĞER export KULLANMIYORSAN BUNA GEREK YOK
-}
-
-module.exports = nextConfig
